0b0d2a7a6ff7280406a0bb692771a29b2a0ec030
gabbyh <gabbyh@users.noreply.github.com>
2017-06-30 17:35:44 +0200

    Export meshes support for ME1/ME2/ME3 - working version but need refactoring

---
+++ Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMeshMorphExporter
+{
+    static class Utils
+    {
+        public static float HalfToFloat(UInt16 val)
+        {
+
+            UInt16 u = val;
+            int sign = (u >> 15) & 0x00000001;
+            int exp = (u >> 10) & 0x0000001F;
+            int mant = u & 0x000003FF;
+            exp = exp + (127 - 15);
+            int i = (sign << 31) | (exp << 23) | (mant << 13);
+            byte[] buff = BitConverter.GetBytes(i);
+            return BitConverter.ToSingle(buff, 0);
+        }
+    }
+}
